886848c0eb4b568179bbbea85989ce6444359a0e
Bradley Arnold <bradley@ngntelecoms.com>
2022-06-02 08:35:50 +0200

    updated sign_up.php

--- includes/sign_up.php
+++ includes/sign_up.php
@@ -1,15 +1,24 @@
 <?php
 include_once 'dbh.inc.php';
 
-$first = $_POST['first'];
-$last = $_POST['last'];
-$email = $_POST['email'];
-$uid = $_POST['uid'];
-$pwd = $_POST['pwd'];
+$first = mysqli_real_escape_string( $conn, $_POST['first']) ;
+$last = mysqli_real_escape_string($conn, $_POST['last']) ;
+$email = mysqli_real_escape_string($conn, $_POST['email']) ; 
+$uid = mysqli_real_escape_string($conn, $_POST['uid']) ;
+$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
-$sql =  "INSERT INTO users(first_name, last_name, email, user_name, password)VALUES('$first', '$last', '$email', '$uid', '$pwd');";
+$sql =  "INSERT INTO users(first_name, last_name, email, user_name, password)VALUES(?,?, ?, ?, ?);";
+// create varible for prepared statments
+$stmt = mysqli_stmt_init($conn);
+// check first errors
+if(!mysqli_stmt_prepare($stmt, $sql)) {
+    echo "SQL error";
+// bind paramaters
+} else {
+    mysqli_stmt_bind_param($stmt, "sssss", $first, $last , $email, $uid , $pwd   );
+    mysqli_stmt_execute($stmt);
+}
 
-mysqli_query($conn, $sql);
 
-header("Location: ../index.php?signup=success");
+header("Location: ../index.html?signup=success");
 ?>
